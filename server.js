03abf11998cea5c043e0ec5436fe9f28d3b81353
AJIT JAIN <ajitkumarjain123@gmail.com>
2016-10-06 19:55:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'one.html'));
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'second.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'two.html'));
 });
 app.get('/article-third', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'third.html'));
