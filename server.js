7d3c9934c5fa4f69ba34d07b3da8fda97e223d8e
AJIT JAIN <ajitkumarjain123@gmail.com>
2016-10-06 19:18:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/first_article', function (req, res) {
+app.get('/first-article', function (req, res) {
   res.send('First article');
 });
 app.get('/', function (req, res) {
