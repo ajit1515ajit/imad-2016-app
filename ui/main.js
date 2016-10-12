369797063cf9ff825bf2ed3170078d2b5c7c0f8b
AJIT JAIN <ajitkumarjain123@gmail.com>
2016-10-12 09:33:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,13 @@ console.log("Now Your Will Changed");
 var element=document.getElementById('main');
 element.innerHTML='AJIT JAIN';
 var img =document.getElementById('madi');
+function moveRight(){
+    marginLeft=marginLeft+10;
+    imag.style.marginLeft=marginLeft+'px';
+    
+}
 img.onclick=function(){
     img.style.marginLeft="100px";
+    var interval=setInterval(moveRight,100);
     
 }
