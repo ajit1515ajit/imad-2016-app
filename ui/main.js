c660d306285382f21cbd689eef934f44e27697e5
AJIT JAIN <ajitkumarjain123@gmail.com>
2016-10-12 10:41:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ img.onclick=function(){
 }*/
 counter=0;
 var button=document.getElementById('counter');
-buttoon.onclick(){
+button.onclick(){
 var req= new XMLHttpRequest();
 req.onreadystatechange(){
     if(req.readystate===XMLHttpRequest.DONE){
