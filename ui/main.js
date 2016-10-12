31e8e257f433dba88d512428101451e2be6c70bd
AJIT JAIN <ajitkumarjain123@gmail.com>
2016-10-12 12:07:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,6 +33,28 @@ req.onreadystatechange=function(){
   req.open('GET','http://ajit1515ajit.imad.hasura-app.io/counter',true);
   res.send(null);
 };
+var submit = document.getElementById('submit_btn');
+submit.onclick=function(){
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+	if(request.readyState===XMLHttpRequest.DONE){
+	if(request.status===200){
+	var names= request.responseText;
+	names=JSON.parse(names);
+	var list='';
+	for(var i=0; i<names.length; i++){
+	list+='<li>'+names[i]+'</li>';
+	}
+	var ul=document.getElementId('namelist');
+	ul.innerHTML=list;
+	}
+	}
+};
+var nameInput=document.getElementBYId('name');
+var name = nameInput.value;
+request.open('GET','http://ajit1515ajit.imad.hasura-app.io/submit-nmae?name='+name,true);
+request.send(null);
+};
 
 /*var button=document.getElementById('counter');
 buttoon.onclick(){
