fc5acd61a49002799a0a90cd99a3e38e6e0932d9
AJIT JAIN <ajitkumarjain123@gmail.com>
2016-10-12 09:20:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
-consol.log("Now Your Will Changed");
+console.log("Now Your Will Changed");
 var element=document.getElementById('main-text');
 element.innerHtml="AJIT JAIN";
