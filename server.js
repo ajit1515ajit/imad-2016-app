4e93d137f6ed74c9156e6e38efc4b05e84ddf83b
AJIT JAIN <ajitkumarjain123@gmail.com>
2016-10-12 10:47:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ function createTemplate(data)
     var htmlTemplate=
          `<html>
 <head>
-	<title>${title}
+	<title>
+	${title}
 	</title>
 	<meta charset="utf-8" name="viewport" content="width-device-width , initial-scale=1">
 	<link rel="stylesheet" href="/ui/style.css">
